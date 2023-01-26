16427edaf7614d2c4618bae801107340e2483334
Ferdousmohammed <ferdousmohammed29@gmail.com>
2023-01-27 01:02:25 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,52 @@
+
+
+ let ferDous = [' Ferose', ' Sadia', 'Farhan', 'Rahman', 'Noyon',];
+
+ferDous[0] = 'Ferdous';
+
+//  document.write(ferDous[0]);
+
+let swftware = [' illustrator', ' photoshop', ' Corel', ' Indesign', ' Figma'];
+
+swftware.push(' Adobe');
+swftware.push('adobe xd');
+swftware.unshift(' Pintarest');
+swftware.unshift(' Tender');
+
+// let position = swftware.indexOf('Figma');
+
+
+
+
+
+
+
+// swftware.length = 0;
+
+// swftware.splice(0,3);
+
+// swftware.shift();
+// swftware.shift();
+
+
+
+    /*swftware.pop();
+swftware.pop();
+swftware.pop();
+swftware.pop();
+swftware.pop();
+swftware.pop();
+swftware.pop();
+swftware.pop();*/
+document.write(swftware);
+
+
+ 
+
+ 
+
+
+
+
+// var subMark = ['bangla','English','Accounting', 'socialogy'];
+// console.log (subMark);
